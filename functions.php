a7d464b934a489637ef2f77da7365afbadbe857b
Lobsang Wangdu <lwangdu@gmail.com>
2017-01-04 19:14:03 -0800

    add style on edit functions

--- functions.php
+++ functions.php
@@ -13,6 +13,8 @@
 load_child_theme_textdomain( 'ibiology' );
 
 add_action( 'genesis_setup', 'ibiology_setup',15 );
+
+
 /**
 *
 *Them setup.
@@ -42,7 +44,7 @@ function ibiology_setup() {
 			'skip-links',
 		) );
 	// Add them support for footer widgets
-	add_theme_support( 'genesis-footer-widgets', 5 );
+	add_theme_support( 'genesis-footer-widgets', 1 );
 	
 	// Unregister other site layouts
 	genesis_unregister_layout( 'content-sidebar-sidebar' );
@@ -56,4 +58,29 @@ function ibiology_setup() {
 	include_once( get_stylesheet_directory() .'/includes/widget-areas.php' );
 	
 	
+}
+
+// Google font stylesheet
+add_action( 'wp_enqueue_scripts', 'ibiology_enqueue_styles' );
+function ibiology_enqueue_styles() {
+	wp_enqueue_style( 'google-fonts','//fonts.googleapis.com/css?family=Roboto:400,400i,700,700i|Signika' );
+	wp_enqueue_style( 'ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
+}
+
+
+/**
+ * Global enqueues
+ *
+ */
+//* Enqueue Scripts and Styles
+add_action( 'wp_enqueue_scripts', 'ibiology_enqueue_scripts_styles' );
+function ibiology_enqueue_scripts_styles() {
+
+	wp_enqueue_script( 'ibiology-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+	$output = array(
+		'mainMenu' => __( 'Menu', 'ibiology' ),
+		'subMenu'  => __( 'Menu', 'ibiology' ),
+	);
+	wp_localize_script( 'ibiology-responsive-menu', 'ibiologyL10n', $output );
+
 }
