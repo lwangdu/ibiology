ddd24e10e2e6243172b0093970477d68ba0a1ddb
Anca Mosoiu <anca@anca.tv>
2017-07-03 15:06:03 -0700

    multi-part video updates

--- wp-content/plugins/ibio-content/templates/single-talk/multi-part-video.php
+++ wp-content/plugins/ibio-content/templates/single-talk/multi-part-video.php
@@ -17,26 +17,30 @@ global $videos;
           $title = "Part $counter: " . $title;
         }
         $titles[ $counter ] = $title;
-        $length = isset( $v[ 'video_length' ] ) ?  '<span class="length">' . esc_attr( $v[ 'video_length' ] ) . '</span>' : '';
+        $length = isset( $v[ 'video_length' ] ) ?  '<span class="length">Duration: ' . esc_attr( $v[ 'video_length' ] ) . '</span>' : '';
         $download = isset( $v[ 'video_download_url' ] ) ?  esc_url( $v[ 'video_download_url' ] ) : '';
         $size = isset( $v[ 'download_size' ] ) ?  '<span class="size">' . esc_attr( $v[ 'download_size' ] ) . '</span>' : '';
         $download_link = ! empty ( $download ) ? "<a href='$download'>Download $size</a>" : '';
-        $transcript = isset( $v[ 'transcript' ] ) ? '<div class="transcript"><div class="toggle">View Transcript</div><div class="content" style="display:none">' . esc_html( $v[ 'transcript' ] ) . '</div></div>' : '';
+        $transcript = isset( $v[ 'transcript' ] ) ? '<span class="transcript toggle" data-toggle="video-part-transcript-'. $counter .'">View Transcript</span><div id="video-part-transcript-'. $counter .'" class="" style="display:none">' . esc_html( $v[ 'transcript' ] ) . '</div>' : '';
         $video_url = isset( $v[ 'video_url' ] ) ? esc_html( $v[ 'video_url' ] ) : '';
+        
+        
         echo "<div class='single-video part-$counter'><h2 class='title'>$title</h2><div class='content'>";
-        echo wp_oembed_get( $video_url , array( 'height' => '450px' ) );
-        echo '</div><div class="footer">';
+        echo wp_oembed_get( $video_url , array( 'height' => 475 ) );
+        echo '</div><div class="footer"><div class="row controls">';
         echo "<span class='video-length' data-length='$length'>$length</span>";
         echo "<span class='video-part-download'><a href='$download'>Download Hi-Res</a></span>";
-        echo "<span class='video-part-transcript-toggle' data-part='$counter'>Transcript</span>";
-        echo "<div id='video-part-transcript-$counter' class='video-part-transcript' style='display:none'>$transcript</div>";
-        echo '</div></div>';
+        echo $transcript;
+        echo '</div></div></div>';
         $counter++;
         
       }
       echo '</div>';
       
+      echo '<div class="videos-info">';
+      
       if ( $num_parts > 1 ){
+        echo '<header>Videos in this Talk</header>';
         $tabs = '<ul class="videos-nav">';
         for( $i = 1 ; $i < $counter ; $i++ ){
           $title =  $titles[ $i ];
@@ -44,7 +48,30 @@ global $videos;
         }
         $tabs .= "</ul>";
         echo $tabs;
+      }  
+      $translations = get_field( 'translations' );
+      $languages = '';
+      if ( is_array( $translations ) ) {
+        $languages .= '<span class="toggle" data-toggle="translations">Translated Versions</span>';
+        $languages .= '<ul class="translations" style="display:none">';
+        foreach( $translations as $t )  {
+          $url = get_permalink( $t->translated_talk );
+          $languages = "<li><a href='$url'>{$t->language}</a></li>";
+        }
+        $languages .= "</ul>";
       }
-            
-    echo '</wrap></section>';
+        
+      $date_recorded = '';
+      $month = get_field( 'date_recorded_month' );
+      $year = get_field( 'date_recorded_year' );
+
+      if ( !empty( $year ) ){
+        $date_recorded = "<div class='date-recorded'>Recorded: $month $year</div>";
+      }
+  
+      echo $date_recorded;
+      echo $languages;
+      
+      echo '</div>';     
+    echo '</div></section>';
   } 
