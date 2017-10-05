10b386568ed2c4cbb0f874f2c4958657a6b33645
Anca Mosoiu <anca@anca.tv>
2017-10-05 11:19:02 -0700

    styling for search results

--- wp-content/themes/ibiology/search.php
+++ wp-content/themes/ibiology/search.php
@@ -8,9 +8,16 @@
 	
 function ibio_setup_result(){
 
-	add_action( 'genesis_entry_content', 'genesis_do_post_image' , 8 );
+    // remove postmeta and postinfo on all search results.
+    remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+    remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
-	global $post;
+    add_action( 'genesis_entry_header', 'ibio_post_type_label', 6);
+
+    add_action( 'genesis_entry_content', 'genesis_do_post_image' , 8 );
+
+
+    global $post;
 	if ( $post->post_type == IBioTalk::$post_type ){
 		$videos = get_field( 'videos' );
 		if ( is_array( $videos ) && count( $videos ) > 1 ){
@@ -21,11 +28,32 @@ function ibio_setup_result(){
 	
 function ibio_search_sidebar(){
 	 dynamic_sidebar( 'sidebar_search' );
-}	
-	
+}
+
+function ibio_search_loop(){
+
+
+}
+
+function ibio_post_type_label(){
+    global $post;
+
+    $type = get_post_type_object($post->post_type);
+    $labels = $type->labels;
+
+    ?>
+
+    <aside class="post-type-label"><?php echo $labels->singular_name; ?></aside>
+
+    <?php
+
+}
+
 /* ------------------------  Page Rendering -----------------*/
 
 add_action( 'genesis_entry_header', 'ibio_setup_result', 5);	
 remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
 add_action( 'genesis_sidebar', 'ibio_search_sidebar' );
+//remove_action( 'genesis_loop', 'genesis_do_loop');
+add_action( 'genesis_loop', 'ibio_search_loop');
 genesis();
