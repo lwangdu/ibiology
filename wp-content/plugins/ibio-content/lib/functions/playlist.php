be773c1d8bf402328e7c4e8675613b5ff0da949e
Anca Mosoiu <anca@anca.tv>
2017-10-11 18:49:02 -0700

    added playlist shortcode

--- wp-content/plugins/ibio-content/lib/functions/playlist.php
+++ wp-content/plugins/ibio-content/lib/functions/playlist.php
@@ -28,12 +28,15 @@ function ibio_talks_playlist($playlist = null, $maxitems = 0){
 
 
     if ( $talks->have_posts( ) ) {
+        $counter = 0;
         echo '<ul class="talks grid">';
         foreach($talks->posts as $t){
+            if ( $maxitems > 0 && $counter >= $maxitems) break;
             global $post;
             $post = $t;
             setup_postdata($post);
             get_template_part( 'parts/list-talk');
+            $counter++;
         }
         echo '</ul>';
     }
@@ -55,4 +58,37 @@ function ibio_compare_playlist_posts( $a, $b){
     } else if ( $a->menu_order < $b->menu_order) {
         return -1;
     }
+}
+
+
+/* Playlist Shortcode.  Use when you want to show the contents of a playlist on a page or post */
+
+add_shortcode( 'ibio_playlist', 'ibio_playlist_shortcode');
+function ibio_playlist_shortcode($atts){
+
+    $atts = shortcode_atts( array (
+        'id' => null,
+        'numtalks' => 4,
+        'start_index' => 0
+        ) , $atts, 'ibio_playlist');
+
+
+    if (empty( $atts['id'])) {
+        return '<span style="color\:red">Please supply a playlist ID to retrieve talks</span>';
+    }
+
+    $playlist = get_post($atts['id']);
+    if ( empty($playlist) || $playlist->post_type != IBioPlaylist::$post_type ){
+        return '<span style="color\:red">Please supply a valid playlist ID to retrieve talks</span>';
+    }
+
+    ob_start();
+
+    ibio_talks_playlist($playlist, $atts['numtalks']);
+
+    $playlist = ob_get_clean();
+
+    return $playlist;
+
+
 }
