f05ea091d256aba66e363093ebd89837d69427a0
Anca Mosoiu <anca@techliminal.com>
2019-07-02 19:02:23 -0700

    removed players

--- wp-content/themes/ibiology/single-ibiology_podcasts.php
+++ wp-content/themes/ibiology/single-ibiology_podcasts.php
@@ -2,6 +2,15 @@
 
 	/* Template for single ibiology podcast. Mostly so we have a sidebar.  */
 
+	function ibio_remove_podcast_embeds(){
+
+		remove_filter('powerpress_player_links', 'powerpressplayer_link_pinw', 30, 3);
+		//remove_filter('powerpress_player', 'powerpressplayer_player_audio', 10, 3); // Audio players (mp3)
+		remove_filter('powerpress_player', 'powerpressplayer_player_video', 10, 3); // Video players (mp4/m4v, webm, ogv)
+		remove_filter('powerpress_player', 'powerpressplayer_player_other', 10, 3); // Audio/Video flv, wmv, wma, oga, m4a and other non-standard media files
+
+	}
+
 	function ibio_podcast_related_items(){
 		//$post = get_queried_object();
 		global $post;
@@ -34,7 +43,9 @@
 	}
 
 
+
 // force full width layout
 add_filter( 'genesis_site_layout', '__genesis_return_content_sidebar' );
+	add_action( 'genesis_header' , 'ibio_remove_podcast_embeds');
 add_action( 'genesis_after_loop', 'ibio_podcast_related_items');
 genesis();
