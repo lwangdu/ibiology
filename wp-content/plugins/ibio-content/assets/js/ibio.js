d895d7f3b78ac537b0dc473a17a8c1dadef80cda
Anca Mosoiu <amosoiu@RWC-MBP-0253.local>
2019-12-05 17:54:29 -0800

    scrolling smoothly to anchro links

--- wp-content/plugins/ibio-content/assets/js/ibio.js
+++ wp-content/plugins/ibio-content/assets/js/ibio.js
@@ -104,6 +104,14 @@ jQuery(document).ready(function($) {
     });
 
 
+	// Smooth scrolling
+	$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
 
 });
 
