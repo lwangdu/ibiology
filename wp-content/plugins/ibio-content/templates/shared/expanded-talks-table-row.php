45ee5415a5786e7a0f15373be94f599af77957ee
Anca Mosoiu <anca@techliminal.com>
2020-05-13 15:45:31 -0700

    tweaking expanded table

--- wp-content/plugins/ibio-content/templates/shared/expanded-talks-table-row.php
+++ wp-content/plugins/ibio-content/templates/shared/expanded-talks-table-row.php
@@ -54,12 +54,12 @@ $speaker_content = '';
     /* Show the single row that shows the name and description of the talk or session. */
 
 ?>
-    <tr><th colspan="<?php echo ($columns - 1) ; ?>" class="section-row">
+    <tr class="section-row"><th colspan="<?php echo ($columns - 1) ; ?>">
             <span class="session-title"><a href="<?php echo $permalink;?>"><?php echo $post->post_title; ?></a></span>
             <p class="description"><?php echo $description; ?></p>
             <p class="speakers-list"><?php echo $speaker_content;?></p>
         </th>
-        <td><?php echo $resources;?></td>
+        <td ><?php echo $resources;?></td>
         </tr>
 <?php
 
